507c73ffd08dfab26b7d7736c91a2452c4760110
mibullfire <mibullfire@mail.com>
2024-10-29 15:28:41 +0100

    29-10-2024

--- Sesión 3/src/Motor.ino
+++ Sesión 3/src/Motor.ino
@@ -1,46 +1,31 @@
+// Código para la primera parte de la práctica.
+
 const int motorPin = 9; // Motor conectado al pin 9
-const int pulsador = 12;
+const int pulsadorPin = 2;
 int marchaParo = LOW;
 int eventoPulsacion = LOW;
-
-int Base;
-float base;
-int Colector;
-float colector;
 
 void setup() {
   pinMode(motorPin, OUTPUT);
   pinMode(pulsadorPin, OUTPUT);
   Serial.begin(9600);
-  //Serial.println("CONTROL MOTOR CC");
-  //Serial.print("Inicialmente, marchaParo = ");
-  //Serial.println(marchaParo);
+  Serial.println("CONTROL MOTOR CC");
+  Serial.print("Inicialmente, marchaParo = ");
+  Serial.println(marchaParo);
 }
 
 void loop() {
-  eventoPulsacion = digitalRead(pulsadorPin); //leemos la pulsacion
+  eventoPulsacion = digitalRead(pulsadorPin); //Leemos la pulsacion
   if(eventoPulsacion == HIGH) {
     if(marchaParo == HIGH) {
       marchaParo = LOW;
     }else{
-      marchaParo = HIGH
+      marchaParo = HIGH;
     }
     digitalWrite(motorPin, marchaParo);
-    //Serial.print("marchaParo =");
-    //Serial.println(marchaParo);
+    Serial.print("marchaParo =");
+    Serial.println(marchaParo);
   }
-
-  Base = analogRead(A0);
-  base = Base * (5.0/1023);
-  Serial.print("Base");
-  Serial.println(base);
-  Colector = analogRead(A1);
-  colector = Colector * (5.0/1023);
-  Serial.print("Colector");
-  Serial.println(colector);
-
   
   delay(300);
 }
-
-
